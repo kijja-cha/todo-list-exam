0e46b4c8e3a75dac0445c60cf04cf16be32cb520
Kijja Charuchinda <kijja.clt@gmail.com>
2024-01-21 23:54:10 +0700

    refactor(TodoList): Rename MainActivity to TodoListActivity

--- app/src/main/java/kijja/amityexam/todolist/TodoListActivity.kt
+++ app/src/main/java/kijja/amityexam/todolist/TodoListActivity.kt
@@ -3,17 +3,23 @@ package kijja.amityexam.todolist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import dagger.hilt.android.AndroidEntryPoint
-import kijja.amityexam.todolist.ui.screen.MainScreen
+import kijja.amityexam.todolist.ui.graph.TodoListGraph
 import kijja.amityexam.todolist.ui.theme.TodoListTheme
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class TodoListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TodoListTheme {
-                MainScreen()
+                Surface(modifier = Modifier.background(Color.White)) {
+                    TodoListGraph()
+                }
             }
         }
     }
